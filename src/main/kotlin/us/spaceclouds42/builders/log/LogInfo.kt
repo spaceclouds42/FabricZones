259d80371f6b6be1f6ef4e9f64bc60a81f30b886
SpaceClouds42 <spaceclouds42@gmail.com>
2021-02-05 00:19:45 -0500

    allow null values

--- src/main/kotlin/us/spaceclouds42/builders/log/LogInfo.kt
+++ src/main/kotlin/us/spaceclouds42/builders/log/LogInfo.kt
@@ -4,13 +4,13 @@ package us.spaceclouds42.builders.log
 class LogInfo(
     private val msg: String,
 
-    private val variables: Array<Any> = arrayOf(),
+    private val variables: Array<Any?> = arrayOf(),
 ) {
     override fun toString(): String {
         val logInfo = msg
 
         for (i in variables.indices) {
-            logInfo.replace("""#${i}""".toRegex(), variables[i].toString())
+            logInfo.replace("""#i""".toRegex(), variables[i].toString())
         }
 
         return logInfo
