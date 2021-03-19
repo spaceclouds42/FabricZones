93817101d1da862bcbcde5ca34ad32f5d2b0a78d
SpaceClouds42 <71787666+SpaceClouds42@users.noreply.github.com>
2021-03-19 19:17:25 -0400

    Update and rename src/main/java/us/spaceclouds42/zones/mixin/FlowableFluidMixin.java to src/main/java/us/spaceclouds42/zones/mixin/griefing/FlowableFluidMixin.java

--- src/main/java/us/spaceclouds42/zones/mixin/griefing/FlowableFluidMixin.java
+++ src/main/java/us/spaceclouds42/zones/mixin/griefing/FlowableFluidMixin.java
@@ -1,4 +1,4 @@
-package us.spaceclouds42.zones.mixin;
+package us.spaceclouds42.zones.mixin.griefing;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.fluid.FlowableFluid;
@@ -16,20 +16,37 @@ import us.spaceclouds42.zones.data.ZoneManager;
 import us.spaceclouds42.zones.data.spec.PosD;
 import us.spaceclouds42.zones.data.spec.Zone;
 
+/**
+ * Prevents fluids from flowing in to/out of zones
+ */
 @Mixin(FlowableFluid.class)
-public class FlowableFluidMixin {
+abstract class FlowableFluidMixin {
+    /**
+     * Blocks fluid flowing if result is in a different zone
+     *
+     * @param blockView world that fluid is in
+     * @param fluidPos block pos of fluid before flowing
+     * @param fluidBlockState block state of fluid
+     * @param flowDirection direction that fluid will flow
+     * @param flowTo block pos of fluid flowing result
+     * @param flowToBlockState block state of fluid flowing result
+     * @param fluidState the fluid state at the flowing result block position before fluid flows
+     * @param fluid fluid type
+     * @param cir callback info returnable
+     */
     @Inject(
-        method = "canFlow",
-        at = @At(
-            value = "TAIL"
-        ),
-        cancellable = true
+            method = "canFlow",
+            at = @At(
+                    value = "TAIL"
+            ),
+            cancellable = true
     )
     private void disallowFlowingBetweenZones(BlockView blockView, BlockPos fluidPos, BlockState fluidBlockState, Direction flowDirection, BlockPos flowTo, BlockState flowToBlockState, FluidState fluidState, Fluid fluid, CallbackInfoReturnable<Boolean> cir) {
         if (!cir.getReturnValue() || !(blockView instanceof World))
             return;
 
         World world = (World) blockView;
+        
         Zone zoneFrom = ZoneManager.INSTANCE.getZone(world, fluidPos);
         Zone zoneTo = ZoneManager.INSTANCE.getZone(world, flowTo);
 
