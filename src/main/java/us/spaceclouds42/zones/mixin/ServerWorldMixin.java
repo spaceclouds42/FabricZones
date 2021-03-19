c0d63d39749305c90d08aab5cb1e90fcfb3e1c63
SpaceClouds42 <71787666+SpaceClouds42@users.noreply.github.com>
2021-03-19 17:09:53 -0400

    javadoc

--- src/main/java/us/spaceclouds42/zones/mixin/ServerWorldMixin.java
+++ src/main/java/us/spaceclouds42/zones/mixin/ServerWorldMixin.java
@@ -9,8 +9,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import us.spaceclouds42.zones.access.BuilderAccessor;
 import us.spaceclouds42.zones.data.BuilderManager;
 
+/**
+ * Ensures that builders cannot stay in builder mode by relogging or teleporting between dimensions
+ */
 @Mixin(ServerWorld.class)
 abstract class ServerWorldMixin {
+    /**
+     * Prevents a crash when joining in singleplayer with a new uuid
+     *
+     * @param player the player that joined
+     * @param ci callback info
+     */
     @Inject(
             method = "onPlayerConnected",
             at = @At(
@@ -23,6 +32,12 @@ abstract class ServerWorldMixin {
         }
     }
 
+    /**
+     * Deactivates builder mode when a player joins, teleports, changes dimension, or respawns
+     *
+     * @param player the player that is being added to the world
+     * @param ci callback info
+     */
     @Inject(
             method = "addPlayer",
             at = @At(
