8a4e5820b728537e1e5230ebf2ddd6cba5d62137
Basique Evangelist <basiqueevangelist@yandex.ru>
2021-03-20 00:29:37 +0300

    Disallow breaking blocks outside/inside of zone

--- src/main/kotlin/us/spaceclouds42/zones/Common.kt
+++ src/main/kotlin/us/spaceclouds42/zones/Common.kt
@@ -3,12 +3,21 @@ package us.spaceclouds42.zones
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents
+import net.minecraft.block.BlockState
+import net.minecraft.block.entity.BlockEntity
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
 import org.apache.logging.log4j.LogManager
+import us.spaceclouds42.zones.access.BuilderAccessor
 import us.spaceclouds42.zones.commands.BuilderCommand
 import us.spaceclouds42.zones.commands.BuilderModeCommand
 import us.spaceclouds42.zones.commands.ZoneCommand
 import us.spaceclouds42.zones.data.BuilderManager
 import us.spaceclouds42.zones.data.ZoneManager
+import us.spaceclouds42.zones.data.ZoneManager.getZone
+import us.spaceclouds42.zones.data.spec.PosD
 import us.spaceclouds42.zones.log.LogMode
 import us.spaceclouds42.zones.log.Logger
 import us.spaceclouds42.zones.utils.Dispatcher
@@ -30,6 +39,39 @@ object Common : ModInitializer {
 
         LOGGER.info("Registering to CommandRegistrationCallback.EVENT event", LogMode.WTF)
         CommandRegistrationCallback.EVENT.register(::registerCommands)
+
+        PlayerBlockBreakEvents.BEFORE.register(::beforeBlockBroken)
+    }
+
+    /**
+     * Cancels block break event if block is in zone and player isn't a builder.
+     */
+    private fun beforeBlockBroken(world: World, player: PlayerEntity, pos: BlockPos, state: BlockState, blockEntity: BlockEntity?): Boolean {
+        check(player is BuilderAccessor)
+
+        val blockZone = getZone(
+            PosD(
+                world.registryKey.value.toString(),
+                pos.x.toDouble(),
+                pos.y.toDouble(),
+                pos.z.toDouble()
+            )
+        )
+
+        if (player.isInBuilderMode) {
+            val playerZone = getZone(
+                PosD(
+                    world.registryKey.value.toString(),
+                    player.x,
+                    player.y,
+                    player.z
+                )
+            )
+
+            return blockZone == playerZone
+        } else {
+            return blockZone == null
+        }
     }
 
     /**
