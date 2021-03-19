42a3c4f08c93a2702f733157a3abd2d4ca152db7
SpaceClouds42 <spaceclouds42@gmail.com>
2021-03-18 22:54:30 -0400

    fix respawning in builder mode bug

---
+++ src/main/java/us/spaceclouds42/zones/mixin/ServerWorldMixin.java
@@ -0,0 +1,25 @@
+package us.spaceclouds42.zones.mixin;
+
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import us.spaceclouds42.zones.access.BuilderAccessor;
+import us.spaceclouds42.zones.data.BuilderManager;
+
+@Mixin(ServerWorld.class)
+abstract class ServerWorldMixin {
+    @Inject(
+            method = "addPlayer",
+            at = @At(
+                    value = "HEAD"
+            )
+    )
+    private void disableBuilderModeOnRespawn(ServerPlayerEntity player, CallbackInfo ci) {
+        if (((BuilderAccessor) player).isInBuilderMode()) {
+            BuilderManager.INSTANCE.getBuilder(player.getUuid()).deactivateBuilderMode(player);
+        }
+    }
+}
