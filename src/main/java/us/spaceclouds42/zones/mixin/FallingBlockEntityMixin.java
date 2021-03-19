9844f3f572d0733a73ec0be368097f30d6cf1c24
Basique Evangelist <basiqueevangelist@yandex.ru>
2021-03-19 22:04:02 +0300

    Add falling block protection
    Makes falling blocks in zones not fall
    Makes falling block entities entering a zone drop as an item

---
+++ src/main/java/us/spaceclouds42/zones/mixin/FallingBlockEntityMixin.java
@@ -0,0 +1,49 @@
+package us.spaceclouds42.zones.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.FallingBlockEntity;
+import net.minecraft.world.GameRules;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import us.spaceclouds42.zones.data.ZoneManager;
+import us.spaceclouds42.zones.data.spec.PosD;
+import us.spaceclouds42.zones.data.spec.Zone;
+
+@Mixin(FallingBlockEntity.class)
+public abstract class FallingBlockEntityMixin extends Entity {
+    @Shadow public boolean dropItem;
+
+    @Shadow private BlockState block;
+
+    public FallingBlockEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
+    private void onTick(CallbackInfo ci) {
+        Zone zone = ZoneManager.INSTANCE.getZone(
+            new PosD(
+                world.getRegistryKey().getValue().toString(),
+                getX(),
+                getY(),
+                getZ()
+            )
+        );
+
+        if (zone != null) {
+            if (dropItem && this.world.getGameRules().getBoolean(GameRules.DO_ENTITY_DROPS)) {
+                dropItem(block.getBlock());
+            }
+
+            discard();
+
+            ci.cancel();
+        }
+    }
+}
