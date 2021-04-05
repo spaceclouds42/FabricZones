02c088ff25d59f00881c051a9cf5d36b7f31146b
SpaceClouds42 <71787666+SpaceClouds42@users.noreply.github.com>
2021-04-05 12:01:54 -0400

    javadoc

--- src/main/java/us/spaceclouds42/zones/mixin/ThreadedAnvilChunkStorageMixin.java
+++ src/main/java/us/spaceclouds42/zones/mixin/ThreadedAnvilChunkStorageMixin.java
@@ -13,13 +13,30 @@ import us.spaceclouds42.zones.data.spec.PosI;
 import us.spaceclouds42.zones.data.spec.Zone;
 import us.spaceclouds42.zones.duck.BuilderAccessor;
 
+/**
+ * Ensures cloaked blocks stay cloaked when update packets are sent
+ */
 @Mixin(ThreadedAnvilChunkStorage.class)
 public class ThreadedAnvilChunkStorageMixin {
-    @Inject(method = "sendChunkDataPackets", at = @At("TAIL"))
+    /**
+     * Hides cloaked blocks in chunk data packets
+     * 
+     * @param player receiver of packet
+     * @param packets the packets being sent
+     * @param chunk the chunk that the packets are sending data of
+     * @param ci callback info
+     */
+    @Inject(
+            method = "sendChunkDataPackets",
+            at = @At(
+                    "TAIL"
+            )
+    )
     private void hideBlocks(ServerPlayerEntity player, Packet<?>[] packets, WorldChunk chunk, CallbackInfo ci) {
-        if (((BuilderAccessor) player).isBuilder())
+        if (((BuilderAccessor) player).isBuilder()) {
             return;
-
+        }
+        
         PosI start = new PosI(
             chunk.getWorld().getRegistryKey().getValue().toString(),
             chunk.getPos().getStartX(),
@@ -32,6 +49,7 @@ public class ThreadedAnvilChunkStorageMixin {
             256,
             chunk.getPos().getEndZ()
         );
+        
         for (Zone zone : ZoneManager.INSTANCE.getCloakedZonesIntersecting(start.toPosD(), end.toPosD())) {
             zone.hideArea(player, start, end);
         }
