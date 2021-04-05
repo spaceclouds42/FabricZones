01f5a21a65ef29bea029ba29a65ce75b15f69f85
SpaceClouds42 <71787666+SpaceClouds42@users.noreply.github.com>
2021-04-05 11:42:27 -0400

    add javadoc, remove unneeded desync protection

--- src/main/java/us/spaceclouds42/zones/mixin/smuggling/ItemStackMixin.java
+++ src/main/java/us/spaceclouds42/zones/mixin/smuggling/ItemStackMixin.java
@@ -15,35 +15,36 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import us.spaceclouds42.zones.duck.BuilderAccessor;
 
+/**
+ * Adds protections against consuming items when in builder mode. Prevents gaining potion effects and resaturating.
+ */
 @Mixin(ItemStack.class)
 public class ItemStackMixin {
     @Shadow @Final private Item item;
 
-    @Inject(method = "finishUsing", at = @At("HEAD"), cancellable = true)
+    /**
+     * Blocks eating and drinnking when in builder mode
+     *
+     * @param w the dimension this occurs in
+     * @param user the entity consuming an item
+     * @param cir returnable callback info
+     */
+    @Inject(
+            method = "finishUsing",
+            at = @At(
+                    "HEAD"
+            ),
+            cancellable = true
+    )
     private void blockEatingAndDrinkingItemsInBuilderMode(World w, LivingEntity user, CallbackInfoReturnable<ItemStack> cir) {
-        if (w.isClient)
+        if (w.isClient) {
             return;
-
+        }
+        
         UseAction action = item.getUseAction((ItemStack)(Object) this);
         if (user instanceof ServerPlayerEntity && ((BuilderAccessor) user).isInBuilderMode() && (action == UseAction.EAT || action == UseAction.DRINK)) {
-            ServerPlayerEntity player = (ServerPlayerEntity) user;
-
-            player.networkHandler.sendPacket(
-                new ScreenHandlerSlotUpdateS2CPacket(
-                    -2,
-                    player.inventory.selectedSlot,
-                    player.getMainHandStack()
-                )
-            );
-            player.networkHandler.sendPacket(
-                new ScreenHandlerSlotUpdateS2CPacket(
-                    -2,
-                    40,
-                    player.getOffHandStack()
-                )
-            );
-
             cir.setReturnValue((ItemStack)(Object) this);
         }
     }
 }
+    
